8f87060181476b4d04e889ea34ce57e98c69ed05
Mikhail Trunov <mikhail.trunov@gmail.com>
2021-07-26 01:59:33 +0300

    Добавил тесты unittest

--- test_cases.py
+++ test_cases.py
@@ -1,3 +1,21 @@
 from main import json_to_db
+import unittest
 
-json_to_db('sample.json')
+
+class TestJsonToDbMethods(unittest.TestCase):
+
+    def test_invalid_json(self):
+        self.assertRaises(Exception, json_to_db, 'errorjson.json')
+
+    def test_file_not_exist(self):
+        self.assertRaises(Exception, json_to_db, '.json')
+
+    def test_wrong_json(self):
+        self.assertRaises(Exception, json_to_db, 'wrongformat.json')
+
+    def test_ok(self):
+        self.assertIsNone(json_to_db('sample.json'))
+
+
+if __name__ == '__main__':
+    unittest.main()
